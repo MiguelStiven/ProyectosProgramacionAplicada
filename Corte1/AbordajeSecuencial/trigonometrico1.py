371b2cc0a76aabc56eb18fbd481953db02c8fc3e
MiguelStiven <msrodriguezh@udistrital.edu.co>
2025-09-04 18:33:33 -0500

    Add files via upload

--- Corte1/AbordajeSecuencial/trigonometrico1.py
+++ Corte1/AbordajeSecuencial/trigonometrico1.py
@@ -2,4 +2,11 @@ import math
 
 radians=float(input("Ingrese un valor en radianes"))
 proyeccion=math.sin(radians)
-print (proyeccion)
+print ("Resultado:", proyeccion) #Para que muestre el seno de los radianes
+
+#No existe alguna funcion para la cual calcule el seno en grados
+
+Grados=radians*(180/math.pi)
+print ("Conversion (Radianes a Grados):", Grados) #Para que muestre la conversion de grados a radianes
+proyeccion2=math.sin(Grados)
+print ("Resultado:", proyeccion2) #Para que muestre el seno de los grados
